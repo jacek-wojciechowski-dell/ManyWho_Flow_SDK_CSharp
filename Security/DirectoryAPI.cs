80ff8f57d1dffc0bf1b2205a18a6f81a308e1a89
Steve Wood <steve.wood@manywho.com>
2016-08-03 05:45:37 -0400

    CORE-2454 - Create Directory Endpoint and add Directory to Who object

--- Security/DirectoryAPI.cs
+++ Security/DirectoryAPI.cs
@@ -1,9 +1,8 @@
-﻿using System;
-using System.Runtime.Serialization;
+﻿using System.Runtime.Serialization;
 
 /*!
 
-Copyright 2013 Manywho, Inc.
+Copyright 2016 Manywho, Inc.
 
 Licensed under the Manywho License, Version 1.0 (the "License"); you may not use this
 file except in compliance with the License.
@@ -20,52 +19,38 @@ permissions and limitations under the License.
 namespace ManyWho.Flow.SDK.Security
 {
     [DataContract(Namespace = "http://www.manywho.com/api")]
-    public class BuilderWhoAPI
+    public class DirectoryAPI
     {
         [DataMember]
-        public String id
+        public string id
         {
             get;
             set;
         }
 
         [DataMember]
-        public String firstName
+        public string developerName
         {
             get;
             set;
         }
 
         [DataMember]
-        public String lastName
+        public string developerSummary
         {
             get;
             set;
         }
 
         [DataMember]
-        public String email
+        public string externalIdentifier
         {
             get;
             set;
         }
 
         [DataMember]
-        public String username
-        {
-            get;
-            set;
-        }
-
-        [DataMember]
-        public string directoryId
-        {
-            get;
-            set;
-        }
-
-        [DataMember]
-        public bool verified
+        public string loginUri
         {
             get;
             set;
